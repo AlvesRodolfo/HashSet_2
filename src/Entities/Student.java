e6fec12e0a806998c3ca0e17c441856b43af3998
Rodolfo Alves <rodolfoalveseng@gmail.com>
2021-09-09 18:07:30 -0300

    Exercise finished

---
+++ src/Entities/Student.java
@@ -0,0 +1,42 @@
+package Entities;
+
+import java.util.Objects;
+
+public class Student {
+	
+	private Long id;
+	
+	public Student() {
+		
+	}
+
+	public Student(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
